4fb3600c0de11413f0e33a27070f836f3d56f997
lukaszcwyl <lukasz.cwyl@commart.pl>
2021-07-23 21:49:37 +0200

    funkcja z return

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//zmienne asekuracyjne
+let firstNumber = 11;
+let secondNumber = 12;
+
+//funkcja przyjmująca dwa parametry i zwracająca ich sumę
+const oblicz = (pierwszaLiczba , drugaLiczba ) =>  pierwszaLiczba + drugaLiczba ;
+//wypisz w konsoli wynik funkcji "oblicz" przekazując do niej argumenty
+console.log(oblicz(firstNumber,secondNumber));
+
